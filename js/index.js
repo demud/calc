d4497f01695a1d5dc19335a39518c575d2cee1ab
Taras <demudt@gmail.com>
2018-06-05 18:12:21 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,16 +1,32 @@
-document.querySelector('.number').addEventListener('click', clickNumber);
-document.querySelector('.operation').addEventListener('click', operation);
+document.querySelectorAll('.number')
+    .forEach( el => el.addEventListener('click', clickNumber) );
+document.querySelector('.plus').addEventListener('click', plus);
+document.querySelector('.minus').addEventListener('click', minus);
+document.querySelector('.divide').addEventListener('click', divide);
+document.querySelector('.multiply').addEventListener('click', multiply);
 document.querySelector('.calc').addEventListener('click', calc);
 const display = document.querySelector('.display');
 
-function clickNumber() {
-    display.value += 1
+function clickNumber(event) {
+    display.value += event.target.innerText;
 }
 
-function operation() {
-    display.value += '+'
+function plus() {
+    display.value += '+';
+}
+
+function minus() {
+    display.value += '-';
+}
+
+function divide() {
+    display.value += '/'
+}
+
+function multiply () {
+    display.value += '*'
 }
 
 function calc() {
-    display.value = eval(display.value)
+    display.value = eval(display.value);
 }
